0d90f07f6979f2f462bdefa45c6cfd71089e638f
root <root@EPBYMINW0492.minsk.epam.com>
2018-10-31 15:15:44 +0300

    initial commit

---
+++ app/tests/app-test.py
@@ -0,0 +1,25 @@
+import sys,os
+sys.path.append(os.getcwd())
+import unittest
+from app.src.app import my_function
+
+
+class MyTest(unittest.TestCase):
+    def test_my_function(self):
+        self.assertEqual(my_function(1, 1), 2)
+        self.assertEqual(my_function(1, -1), 0)
+        self.assertEqual(my_function(0, 0), 0)
+        self.assertEqual(my_function(-1, -1), -2)
+        self.assertEqual(my_function(1.0, 1), 2)        
+        self.assertEqual(my_function(1.1, 1.1), 2.2)        
+
+class MyTestWhatShouldFail(unittest.TestCase):
+    def test_my_function(self):
+        # 1 + 1 = 4
+        self.assertEqual(my_function(1, 1), 4)
+                                    
+                                            
+                                                                            
+                                                                                    
+if __name__ == '__main__':
+    unittest.main()
